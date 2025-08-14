4cf07b0d31309cc16cee770ee28385df8ba1104e
byronRC10 <byronredondo29@gmail.com>
2025-08-14 09:25:27 -0600

    Test complete

---
+++ OldPhonePadC/Data.cs
@@ -0,0 +1,39 @@
+﻿
+namespace OldPhonePadC
+{
+    internal class Data
+    {
+        public static readonly Dictionary<char, char[]> Keys = new Dictionary<char, char[]>
+        {
+            { '1', new char[] { '&', '\'', '(', ')' } },
+            { '2', new char[] { 'A', 'B', 'C' } },
+            { '3', new char[] { 'D', 'E', 'F' } },
+            { '4', new char[] { 'G', 'H', 'I' } },
+            { '5', new char[] { 'J', 'K', 'L' } },
+            { '6', new char[] { 'M', 'N', 'O' } },
+            { '7', new char[] { 'P', 'Q', 'R', 'S' } },
+            { '8', new char[] { 'T', 'U', 'V' } },
+            { '9', new char[] { 'W', 'X', 'Y', 'Z' } },
+            { '0', new char[] { ' ' } },
+            { '*', new char[] { '*' } },
+            { '#', new char[] { '#' } }
+        }; // data for simulate 
+
+        public static char[] GetKeyChars(char key)
+        {
+            if (Keys.ContainsKey(key))
+                return Keys[key];
+            return new char[0];
+        }// get key
+
+        public static char GetCharAt(char key, int index)
+        {
+            var chars = GetKeyChars(key);
+            if (index >= 0 && index < chars.Length)
+                return chars[index];
+            return '\0'; 
+        }// get char
+
+    }// class
+
+}// namesp
---
+++ OldPhonePadC/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace OldPhonePadC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start typing (ESC to exit):");
+
+            string output = "";
+            DateTime lastKeyTime = DateTime.Now;
+            TimeSpan waitTime = TimeSpan.FromSeconds(1);
+
+            while (true)
+            {
+                var keyInfo = Console.ReadKey(true); 
+
+                if (keyInfo.Key == ConsoleKey.Escape)
+                    break;
+
+                DateTime now = DateTime.Now;
+
+                if (now - lastKeyTime > waitTime)
+                {
+                    output += " ";
+                    Console.Write(" ");
+                }
+
+                output += keyInfo.KeyChar;
+                Console.Write(keyInfo.KeyChar); 
+                lastKeyTime = now;
+            }
+
+            string processed = Functions.ProcessOldPhonePad(output);
+            Console.WriteLine("Processed by Functions: " + processed);
+        }// main
+
+    }// class
+
+}// namesp
+
